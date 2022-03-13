d17982df4d9f62bf9017c01fe1ff4f9021a01cec
BPMinaker <bminaker@gmail.com>
2022-03-13 15:26:53 -0400

    new parameter values

--- fixed_specs.jl
+++ fixed_specs.jl
@@ -1,15 +1,13 @@
-
-mass = 1600
+t = 1.6
+wb = 2.8
+hg = 0.5
+mass = 1800
 Ix = 800
 Iy = 2000
 Iz = 2200
-hg = 0.5
-wb = 2.6
-
-cod = 0.35
-farea = 3.2
 drive = "RearWheelDrive"
+farea = 3.5
+cod = 0.35
 
 mu = 40
 kt = 150000
-t = 1.5
