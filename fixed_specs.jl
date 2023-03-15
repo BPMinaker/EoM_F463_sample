02f6c3c21b26c970c30547a6ee5ee27052ecd57d
BPMinaker <bminaker@uwindsor.ca>
2023-03-15 15:29:47 -0400

    2023 competition update

--- fixed_specs.jl
+++ fixed_specs.jl
@@ -1,13 +1,13 @@
-t = 1.6
-wb = 2.8
-hg = 0.5
-mass = 1800
-Ix = 800
-Iy = 2000
-Iz = 2200
+t = 1.52
+wb = 2.63
+hg = 0.51
+mass = 1371
+Ix = 461
+Iy = 1848
+Iz = 2045
 drive = "RearWheelDrive"
-farea = 3.5
-cod = 0.35
+farea = 2.0
+cod = 0.32
 
 mu = 40
 kt = 150000
